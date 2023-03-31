1a1c092838a1fec303ba9806490dc2485d7e6d77
Songlin Yang <511121939@qq.com>
2023-03-31 16:05:20 +0800

    feat(*): initialize template project (#1)
    Signed-off-by: lsytj0413 <511121939@qq.com>

---
+++ hack/read_cpus_available.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Copyright (c) 2023 The Songlin Yang Authors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+set -e
+
+if [ -z "$MAX_CPUS" ]; then
+    MAX_CPUS=1
+
+    case "$(uname -s)" in
+    Darwin)
+        MAX_CPUS=$(sysctl -n machdep.cpu.core_count)
+        ;;
+    Linux)
+        CFS_QUOTA=$(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us)
+        if [ "$CFS_QUOTA" -ge 100000 ]; then
+            MAX_CPUS=$(("$CFS_QUOTA" / 100 / 1000))
+        fi
+        ;;
+    *)
+        # Unsupported host OS. Must be Linux or Mac OS X.
+        ;;
+    esac
+fi
+
+echo "$MAX_CPUS"
